f401b066fb19ca998a51c7a8bf5f7f068d7a4dd0
meu29 <umeumeume803@gmail.com>
2021-03-10 13:02:08 +0900

    generics

---
+++ その他/generics_practice.js
@@ -0,0 +1,37 @@
+/* 引数をそのまま返す関数2つ(引数の型がnumber用とstring用) */
+var test = function (arg) {
+    return arg;
+};
+var test2 = function (arg) {
+    return arg;
+};
+console.log(test(1));
+console.log(test2("あああ"));
+/* genericを使用 */
+/* 型の大文字に決まりはない */
+var test_by_generics = function (arg) {
+    return arg;
+};
+/* 一つの関数で異なる型を扱える */
+/* Tにnumberを代入 */
+console.log(test_by_generics(1));
+/* Tにstringを代入 */
+console.log(test_by_generics("あああ"));
+var GenericClass = /** @class */ (function () {
+    function GenericClass(old_item, new_item) {
+        this.old_item = old_item;
+        this.new_item = new_item;
+    }
+    GenericClass.prototype.getOldItem = function () {
+        return this.old_item;
+    };
+    GenericClass.prototype.getNewItem = function () {
+        return this.new_item;
+    };
+    return GenericClass;
+}());
+var genericClass_instance = new GenericClass(1, "あああ");
+/* getOldItem()でnumber型の値を返すようになる */
+console.log(genericClass_instance.getOldItem());
+/* getNewItem()でstring型の値を返すようになる */
+console.log(genericClass_instance.getNewItem());

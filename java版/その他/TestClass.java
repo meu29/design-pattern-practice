a7efa9dc8a676aa57e46451be7f7578e251e6f83
meu29 <umeumeume803@gmail.com>
2021-03-15 11:43:19 +0900

    java継承

--- java版/その他/TestClass.java
+++ java版/その他/TestClass.java
@@ -8,7 +8,12 @@ class TestClass {
         System.out.println(user1.userId);
         user1.changePasscode(334667, 112233);
         user1.changePasscode(334668, 112233);
-        
+
+        Administrator user2 = new Administrator("@ziro4491", 37564);
+        user2.changeUserId("@saburo888");
+        System.out.println(user2.userId);
+        user2.changePasscode(6256);
+
     }
 
 }
@@ -16,8 +21,7 @@ class TestClass {
 class User {
 
     public String userId;
-    /* privateメンバなのでクラス外からの参照不可 */
-    private Number passcode;
+    protected Number passcode;
 
     /* コンストラクタ -> クラスと同じ名前の関数 */
     User(String userId, Number passcode) {
@@ -36,5 +40,26 @@ class User {
     
     }
 
+
+}
+
+class Administrator extends User {
+
+    Administrator(String userId, Number passcode) {
+        super(userId, passcode);
+    }
+
+    /* 子クラスだけが持つクラス */
+    void changeUserId(String newUserId) {
+        this.userId = newUserId;
+    }
+
+    /* オーバーライド(親クラスのメソッドを再定義 親クラスのメソッドには影響なし) */
+    /* 古いパスワードの認証なしに新しくパスワードを設定できるようにする */
+    void changePasscode(Number new_passcord) {
+        /* passcodeがprivateメンバならthis.passcodeはできない */
+        this.passcode = new_passcord;
+        System.out.println("パスコードを" + this.passcode.toString() + "に変更しました");
+    }
 
 }
